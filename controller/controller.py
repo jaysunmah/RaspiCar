33d96f69e8439711a6bdf964087c844268c11231
Nikhil Birmiwal <nbirmiwa@andrew.cmu.edu>
2017-04-15 00:17:51 -0400

    first implementation of connection with internet

--- controller/controller.py
+++ controller/controller.py
@@ -1,12 +1,21 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from socket import * # Communicate with the CAR
 import RPi.GPIO as GPIO
 
 gpio_fwd = 26
 gpio_back = 19
 gpio_left = 21
 gpio_right = 20 
+gpio_pandown = 6
+
+HOST = '128.237.232.190'    # Server(Raspberry Pi) IP address
+PORT = 21567
+BUFSIZ = 1024             # buffer size
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)   # Create a socket
+tcpCliSock.connect(ADDR)                    # Connect with the server
 
 GPIO.setmode(GPIO.BCM)
 
@@ -15,14 +24,28 @@ GPIO.setup(gpio_back, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(gpio_fwd, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(gpio_left, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(gpio_right, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+GPIO.setup(gpio_pandown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while 1:
+  moveForward = False
+  moveBackward = False
   if GPIO.input(gpio_fwd):
     print 'FORWARDS'
-  if GPIO.input(gpio_back):
+    moveForward = True
+  elif GPIO.input(gpio_back):
     print 'BACKWARDS'
+    moveBackward = True
+
   if GPIO.input(gpio_left):
     print 'LEFT'
   if GPIO.input(gpio_right):
     print 'RIGHT'
+  if GPIO.input(gpio_pandown):
+    print 'RIGHT'
+
+  if moveForward:
+    tcpCliSock.send('forward')
+  elif moveBackward:
+    tcpCliSock.send('backward')
+  else: 
+    tcpCliSock.send('stop')
