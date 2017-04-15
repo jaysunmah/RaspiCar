535e620aa9583060da7d8ee8554575855aaa0475
Nikhil Birmiwal <nbirmiwa@andrew.cmu.edu>
2017-04-14 23:31:10 -0400

    can respond to button presses up/down left/right

--- controller/controller.py
+++ controller/controller.py
@@ -1,1 +1,28 @@
-sample text
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+
+gpio_fwd = 26
+gpio_back = 19
+gpio_left = 21
+gpio_right = 20 
+
+GPIO.setmode(GPIO.BCM)
+
+# setup GPIO pins
+GPIO.setup(gpio_back, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gpio_fwd, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gpio_left, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gpio_right, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+while 1:
+  if GPIO.input(gpio_fwd):
+    print 'FORWARDS'
+  if GPIO.input(gpio_back):
+    print 'BACKWARDS'
+  if GPIO.input(gpio_left):
+    print 'LEFT'
+  if GPIO.input(gpio_right):
+    print 'RIGHT'
